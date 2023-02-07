33b991e36858bb2c8d34aa55cbe28d9e3d3ed3c4
Keshav Rambhujoo <kr@valstream.com>
2023-02-07 08:28:48 +0000

    Create User + Hash Password

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from sqlalchemy.orm import Session
-from app import models
+from app import models, schemas, utils
 from app.database import engine, get_db
-from app.schemas import PostCreate, Post
 from typing import List
 
 
@@ -16,13 +15,13 @@ def root():
     return {"message": "Hello World!!!"}
 
 
-@app.get("/posts", response_model=list[Post])
+@app.get("/posts", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
     return posts
 
 
-@app.get("/posts/{id_}", response_model=Post)
+@app.get("/posts/{id_}", response_model=schemas.Post)
 def get_post(id_: int, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == id_).first()
 
@@ -33,8 +32,8 @@ def get_post(id_: int, db: Session = Depends(get_db)):
     return post
 
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=Post)
-def create_post(post: PostCreate, db: Session = Depends(get_db)):
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
     post = models.Post(**post.dict())
     db.add(post)
     db.commit()
@@ -56,8 +55,8 @@ def delete_post(id_: int, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@app.put("/posts/{id_}", response_model=Post)
-def update_post(id_: int, updated_post: PostCreate, db: Session = Depends(get_db)):
+@app.put("/posts/{id_}", response_model=schemas.Post)
+def update_post(id_: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
     post_query = db.query(models.Post).filter(models.Post.id == id_)
     post = post_query.first()
 
@@ -69,3 +68,13 @@ def update_post(id_: int, updated_post: PostCreate, db: Session = Depends(get_db
     db.commit()
 
     return post_query.first()
+
+
+@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    user.password = utils.hash_password(user.password)
+    user = models.User(**user.dict())
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
--- app/models.py
+++ app/models.py
@@ -4,9 +4,16 @@ from app.database import Base
 
 class Post(Base):
     __tablename__ = "posts"
-
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default="TRUE", nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("NOW()"))
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("NOW()"))

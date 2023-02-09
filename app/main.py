746594239b64b440f9d3c8f110e19527c3507d6c
Keshav Rambhujoo <kr@valstream.com>
2023-02-09 16:51:37 +0000

    DB Stuff / Migrations Only Done By Alembic

--- app/main.py
+++ app/main.py
@@ -1,10 +1,6 @@
 from fastapi import FastAPI
-from app import models
-from app.database import engine
 from app.routers import post, user, auth, vote
 
-
-models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(post.router)

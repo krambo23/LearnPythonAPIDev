4bbe7cf9f72c6957dbed150ebc942baf85f03b7d
Keshav Rambhujoo <kr@valstream.com>
2023-02-09 13:58:30 +0000

    Added Alembic

--- app/models.py
+++ app/models.py
@@ -9,6 +9,7 @@ class User(Base):
     email = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("NOW()"))
+    # phone_number = Column(String)
 
 
 class Post(Base):

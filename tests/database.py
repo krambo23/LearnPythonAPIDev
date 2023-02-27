ba6e73c2fe0da54e2077be49175b0ab76f24d177
Keshav Rambhujoo <23krambo@gmail.com>
2023-02-27 21:05:34 +0000

    Test DB Fixtures Moved to Seperate File

---
+++ tests/database.py
@@ -0,0 +1,42 @@
+import pytest
+from app.database import get_db, Base
+from app.main import app
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.config import settings
+from fastapi.testclient import TestClient
+
+
+SQLACHEMY_DATABASE_URL = f"postgresql://{settings.DB_USER}:" \
+                         f"{settings.DB_PASS}@{settings.DB_IP}:" \
+                         f"{settings.DB_PORT}/{settings.DB_NAME}_test"
+
+engine = create_engine(SQLACHEMY_DATABASE_URL)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@pytest.fixture
+def session():
+    print("\nDropping Tables")
+    Base.metadata.drop_all(bind=engine)
+
+    print("\nCreating Tables")
+    Base.metadata.create_all(bind=engine)
+
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture
+def client(session):
+    def get_test_db():
+        try:
+            yield session
+        finally:
+            session.close()
+
+    app.dependency_overrides[get_db] = get_test_db
+    yield TestClient(app)
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,43 +1,16 @@
-from fastapi.testclient import TestClient
 from fastapi import status
-from app.database import get_db, Base
-from app.main import app
 from app import schemas
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from app.config import settings
+from tests.database import client, session
 
 
-SQLACHEMY_DATABASE_URL = f"postgresql://{settings.DB_USER}:" \
-                         f"{settings.DB_PASS}@{settings.DB_IP}:" \
-                         f"{settings.DB_PORT}/{settings.DB_NAME}_test"
-
-engine = create_engine(SQLACHEMY_DATABASE_URL)
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base.metadata.create_all(bind=engine)
-
-
-def get_test_db():
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-app.dependency_overrides[get_db] = get_test_db
-
-client = TestClient(app)
-
-
-def test_root():
+def test_root(client, session):
     res = client.get("/")
 
     assert res.json().get("message") == "Hello World!!! - From Docker Container"
     assert res.status_code == status.HTTP_200_OK
 
 
-def test_create_user():
+def test_create_user(client):
     res = client.post("/users", json={
         "email": "test@example.com",
         "password": "12345"

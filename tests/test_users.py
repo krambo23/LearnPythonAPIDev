5dd907d80ef7bce549122fbc468a07cbbe112650
Keshav Rambhujoo <23krambo@gmail.com>
2023-02-28 08:53:18 +0000

    Test Token

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,7 +1,23 @@
+import pytest
 from fastapi import status
 from app import schemas
 from tests.database import client, session
 
+
+@pytest.fixture
+def test_user(client):
+    user_data = {
+        "email": "user@email.com",
+        "password": "123"
+    }
+
+    res = client.post("/users", json=user_data)
+
+    assert res.status_code == status.HTTP_201_CREATED
+
+    new_user = res.json()
+    new_user["password"] = user_data["password"]
+    return new_user
 
 def test_root(client, session):
     res = client.get("/")
@@ -20,3 +36,12 @@ def test_create_user(client):
 
     assert res.status_code == status.HTTP_201_CREATED
     assert test_user.email == "test@example.com"
+
+
+def test_login_user(client, test_user):
+    res = client.post("/login", data={
+        "username": test_user["email"],
+        "password": test_user["password"]
+    })
+
+    assert res.status_code == status.HTTP_200_OK

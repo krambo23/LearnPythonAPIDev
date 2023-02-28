b545728f795ef16067eb32a879368cc333b17dda
Keshav Rambhujoo <23krambo@gmail.com>
2023-02-28 15:30:33 +0000

    Testing - Unauthorised User - Post Retrieval

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -13,3 +13,28 @@ def test_get_all_posts(authorised_client, test_posts):
 
     assert res.status_code == status.HTTP_200_OK
     assert len(res.json()) == len(test_posts)
+
+
+def test_get_one_post(authorised_client, test_posts):
+    res = authorised_client.get(f"/posts/{test_posts[0].id}")
+    post = schemas.PostOut(**res.json())
+
+    assert res.status_code == status.HTTP_200_OK
+    assert post.Post.id == test_posts[0].id
+    assert post.Post.content == test_posts[0].content
+
+
+def test_unauthorised_user_get_all_posts(client, test_posts):
+    res = client.get("/posts")
+    assert res.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+def test_unauthorised_user_get_one_post(client, test_posts):
+    res = client.get(f"/posts/{test_posts[0].id}")
+    assert res.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+def test_get_non_existent_post(authorised_client, test_posts):
+    res = authorised_client.get("/posts/696969")
+    assert res.status_code == status.HTTP_404_NOT_FOUND
+
